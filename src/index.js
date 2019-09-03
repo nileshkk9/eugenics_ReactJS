b976ef3e821857d30f1f0e3ba749a167eb7598bc
nileshkk9 <nileshkk9@gmail.com>
2019-09-03 19:18:00 +0530

    offline

--- src/index.js
+++ src/index.js
@@ -34,4 +34,5 @@ ReactDOM.render(routing, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-serviceWorker.register();
+// serviceWorker.register();
+
